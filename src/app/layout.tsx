31ecd5313948a21f8707bd1f34d998d7d9927518
Nuno Teixeira <nuno.m.s.teixeira@gmail.com>
2026-03-07 23:31:00 +0000

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const jakarta = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
@@ -36,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en-GB">
+    <html lang="en-GB" className={cn("font-sans", geist.variable)}>
       <body className={`${jakarta.variable} antialiased`}>{children}</body>
     </html>
   );
